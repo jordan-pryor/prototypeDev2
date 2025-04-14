d849331ba5f2015781d37b56e9f7636cc2382b41
Tyson Grant <TacitTempo@gmail.com>
2025-04-14 06:08:47 -0500

    Interact added
    -Added new playerInteract script
    -Added UI canvas
    -Added interact prompt
    -Made UI prefab
    -Updated Player prefab with interact
    -Made interactable layer
    -Updated locker prefabs with Interactable layer

---
+++ Assets/Scripts/player_scripts/playerInteract.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class playerInteract : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] float range = 3f;
+    [SerializeField] LayerMask mask;
+    [SerializeField] GameObject prompt;
+
+    IInteract target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInteractable();
+        if(target != null && Input.GetKeyDown(KeyCode.E))
+        {
+            target.Interact();
+        }
+    }
+    void CheckInteractable()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, range, mask))
+        {
+            IInteract interactable = hit.collider.GetComponent<IInteract>();
+            if(interactable != null)
+            {
+                if (target != interactable)
+                {
+                    target = interactable;
+                    prompt.SetActive(true);
+                }
+                return;
+            }
+        }
+        target = null;
+        prompt.SetActive(false);
+    }
+}

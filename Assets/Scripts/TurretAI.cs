ec9aadc57b84066b75e2bad9d39a4850b3c6a29d
RoxanneKilgannon <rekilgannon@student.fullsail.edu>
2025-04-27 19:21:25 -0500

    Created a "turret" enemy type
    I created a turret prefab that will look to the player, and will fire if they're in range. It can be shot to break it. I also made a turret bullet for the turret to fire.

---
+++ Assets/Scripts/TurretAI.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TurretAI : MonoBehaviour, IDamage
+{
+    [Header("-----Components------")]
+    [SerializeField] Renderer model;
+    [SerializeField] Transform turretEyePosition;
+    [SerializeField] GameObject bullet;
+
+    [Header("-----Stats-----")]
+    [Range(1,3)][SerializeField] int HP;
+    [Range(1,10)][SerializeField] int faceTargetSpeed;
+    [Range(45,180)][SerializeField] int FOVangle;
+    [Range(2,10)][SerializeField] int shootRate;
+    
+
+    float shootTimer;
+    bool PlayerInRange;
+    float angleToPlayer;
+
+    Color colorOrig;
+    Vector3 playerDirection;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerInRange && canSeePlayer())
+        {
+            if(shootTimer >= shootRate)
+            {
+                shoot();
+            }
+        }
+    }
+
+    bool canSeePlayer()
+    {
+        playerDirection = (GameManager.instance.player.transform.position - turretEyePosition.position);
+        angleToPlayer = Vector3.Angle(new Vector3(playerDirection.x, playerDirection.y, playerDirection.z), transform.forward);
+
+        RaycastHit hit;
+        if(Physics.Raycast(turretEyePosition.position, playerDirection, out hit))
+        {
+            if(hit.collider.CompareTag("Player") && angleToPlayer <= FOVangle)
+            {
+                faceTarget();
+
+                shootTimer += Time.deltaTime;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, turretEyePosition.position, transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInRange = false;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashRed());
+
+        if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDirection.x, playerDirection.y, playerDirection.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+}

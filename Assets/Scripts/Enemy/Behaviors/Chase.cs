e513880aef37da16cb85a711cebc3a498ddeb57d
Tyson Grant <TacitTempo@gmail.com>
2025-04-29 13:12:35 -0500

    AI
    -tweaked AI

--- Assets/Scripts/Enemy/Behaviors/Chase.cs
+++ Assets/Scripts/Enemy/Behaviors/Chase.cs
@@ -5,26 +5,35 @@ public class Chase : EnemyBehavior, IEnemy
 {
     public override void Execute(EnemyController controller)
     {
-    
-        Transform target = GameManager.instance.player.transform;
-        if (controller.seenPlayer)
-        {
-            controller.agent.SetDestination(target.position);
-            float distance = Vector3.Distance(controller.transform.position, target.position);
-            controller.animator.SetBool("isWalking", true);
-            if (distance <= controller.agent.stoppingDistance + 0.1f)
-            {
-                controller.animator.SetBool("isWalking", false);
-                controller.SetBehavior(EnemyController.Behavior.Action);
-            }
-        }
-        else
+        Debug.Log("Move");
+        if (!controller.seenPlayer)
         {
             if (controller.memoryTimer <= 0f)
             {
+                controller.agent.isStopped = true;
+                controller.agent.ResetPath();
                 controller.animator.SetBool("isWalking", false);
                 controller.SetBehavior(EnemyController.Behavior.Search);
             }
+            return;
+        }
+
+        Transform target = GameManager.instance.player.transform;
+        controller.agent.stoppingDistance = controller.actionRange;
+        float distance = Vector3.Distance(controller.transform.position, target.position);
+
+        if (distance > controller.actionRange)
+        {
+            controller.agent.isStopped = false;
+            controller.agent.SetDestination(target.position);
+            controller.animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            controller.agent.isStopped = true;
+            controller.agent.ResetPath();
+            controller.animator.SetBool("isWalking", false);
+            controller.SetBehavior(EnemyController.Behavior.Action);
         }
     }
 }
